dd1b12595ba5ed873f10ebf9fb9628c961f95bc9
matvey <matvey@DESKTOP-F99F9V3>
2018-10-16 22:58:50 +0500

    Created bot

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,27 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Main {
+	public static void main(String[] args) {
+		// Initialize Api Context
+		System.getProperties().put("proxySet", "true");
+		System.out.println("1");
+		System.getProperties().put("socksProxyHost", "127.0.0.1");
+		System.out.println("1");
+		System.getProperties().put("socksProxyPort", "1337");
+		System.out.println("1");
+		ApiContextInitializer.init();
+		System.out.println("1");
+		System.out.println("Hekllo!");
+		// Instantiate Telegram Bots API
+		MyAmazingBot bot = new MyAmazingBot();
+		TelegramBotsApi botsApi = new TelegramBotsApi();
+		try {
+			System.out.println("trying");
+			botsApi.registerBot(bot);
+		} catch (TelegramApiException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ HelloWorld/src/MyAmazingBot.java
@@ -0,0 +1,35 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class MyAmazingBot extends TelegramLongPollingBot {
+	@Override
+	public void onUpdateReceived(Update update) {
+
+		// We check if the update has a message and the message has text
+		if (update.hasMessage() && update.getMessage().hasText()) {
+			// Set variables
+			String message_text = update.getMessage().getText();
+			long chat_id = update.getMessage().getChatId();
+
+			SendMessage message = new SendMessage() // Create a message object object
+					.setChatId(chat_id).setText(message_text);
+			try {
+				execute(message); // Sending our message object to user
+			} catch (TelegramApiException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Override
+	public String getBotUsername() {
+		return "translate1337_bot";
+	}
+
+	@Override
+	public String getBotToken() {
+		return "678358194:AAEtrqdE7cU44FUKSDelchq51s8Wq0oq1FI";
+	}
+}
