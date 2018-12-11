10ca5e9bf3fd1467a71a7fdc20cc6bd147bb05a4
Jubick <mattyson.so@gmail.com>
2018-12-11 15:00:06 +0500

    Removed toLowerCase instruction which broek everrything

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-       /* var a = "примочка";
-        var t = new NewTranslator();
-        var b = t.translate(a);
-        var c = t.translate(b);
-        System.out.println(b + " " + c);
-
-        */BotInit botInit = new BotInit();
+        BotInit botInit = new BotInit();
         botInit.create();
     }
 }
